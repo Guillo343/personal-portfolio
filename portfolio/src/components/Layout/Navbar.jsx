65cb4d0fc8c1a8c344d1b42bee7cdf1cdbaf27bb
Guillermo Muñoz <guillemunozi2003@gmail.com>
2025-03-20 12:33:40 -0500

    Summary

--- portfolio/src/components/Layout/Navbar.jsx
+++ portfolio/src/components/Layout/Navbar.jsx
@@ -36,6 +36,7 @@ const Navbar = () => {
     { name: 'About', href: '#about' },
     { name: 'Projects', href: '#projects' },
     { name: 'Skills', href: '#skills' },
+    { name: 'Experience', href: '#experience' },
     { name: 'Contact', href: '#contact' },
   ];
 
